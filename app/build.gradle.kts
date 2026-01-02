2283c0bd1aafee536c4b5612ba8d7b708c386f0c
Monu Kumar <monuk7735@gmail.com>
2026-01-03 00:17:19 +0530

    fix: readme + version + changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 23
         targetSdk = 34
         versionCode = 21
-        versionName = "2.1-beta"
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
